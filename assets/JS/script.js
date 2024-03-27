dee835cc095f5976d78f1b8015e5cdc09bf89b14
Kauê Lima <kaue5939.lima@gmail.com>
2024-03-27 01:42:48 -0300

    Initial commit

---
+++ assets/JS/script.js
@@ -0,0 +1,26 @@
+const botoes = document.querySelectorAll(".botao");
+
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, index) => {
+  botao.addEventListener("click", () => {
+    desselecionarBotao();
+    desselecionarPersonagem();
+
+    botao.classList.add("selecionado");
+
+    personagens[index].classList.add("selecionado");
+  });
+});
+
+function desselecionarPersonagem() {
+  const personagemSelecionado = document.querySelector(
+    ".personagem.selecionado"
+  );
+  personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+  const botaoSelecionado = document.querySelector(".botao.selecionado");
+  botaoSelecionado.classList.remove("selecionado");
+}
